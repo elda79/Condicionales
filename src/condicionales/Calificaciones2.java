b1a436762f23bc837256f0c101078544b1887b7f
negrita79 <elisa.79@live.cl>
2017-11-03 20:18:38 -0300

    Nota 7 notable

--- src/condicionales/Calificaciones2.java
+++ src/condicionales/Calificaciones2.java
@@ -4,11 +4,22 @@
  * and open the template in the editor.
  */
 package condicionales;
-
+import javax.swing.JOptionPane;
 /**
  *
  * @author LAB05
  */
-public class Calificaciones2 {
-    
+public class Calificaciones2 
+{
+ public static void main(String[] args)
+ {
+  float nota;
+   nota1 =Float.parseFloat(JOptionPane.showInputDialog("Ingrese una nota"));
+   if (nota == 7)
+   {
+     JOptionPane.showMessageDialog(null, "Notable");  
+   }
+   
+ }
+         
 }
